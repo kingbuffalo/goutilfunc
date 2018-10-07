f5e02f5b4df888359c0cdf545cbc3cc3cf75ef2a
chendongsheng <chendongsheng@heywoods.cn>
2018-10-07 10:35:02 +0800

    a

--- utilfunc.go
+++ utilfunc.go
@@ -2,18 +2,31 @@ package goutilfunc
 
 import (
 	"encoding/json"
+	"github.com/kingbuffalo/seelog"
+	"runtime"
 )
 
 func PanicJsonUnmarshalError(b []byte, err error) {
 	se := err.(*json.SyntaxError)
-	s := se.Offset - 30
-	if s < 0 {
-		s = 0
+	if se != nil {
+		s := se.Offset - 30
+		if s < 0 {
+			s = 0
+		}
+		e := se.Offset + 30
+		if int(e) > len(b) {
+			e = int64(len(b))
+		}
+		errMsg := string(b[s:e])
+		panic(errMsg)
 	}
-	e := se.Offset + 30
-	if int(e) > len(b) {
-		e = int64(len(b))
+}
+
+func RecoverPanic() {
+	if err := recover(); err != nil {
+		_ = seelog.Error(err)
+		buf := make([]byte, 1<<11)
+		runtime.Stack(buf, true)
+		_ = seelog.Error(string(buf))
 	}
-	errMsg := string(b[s:e])
-	panic(errMsg)
 }
